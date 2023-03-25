3958c2b0ceac7dedbfad4f5e81d3fed24db55057
Sage Watson <sagewatson@developersinstitute.co.nz>
2023-03-25 21:20:40 +1300

    Updated photo path for build

--- components/About.js
+++ components/About.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import sageImg from "../public/assets/sage.JPG";
+import sageImg from "../public/assets/sage-pic.jpg";
 import Image from "next/image";
 
 const About = () => {
