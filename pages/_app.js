c60f672461403e3ded6a087a40a89c3be0730103
Sage Watson <sagewatson@developersinstitute.co.nz>
2023-04-27 19:29:17 +1200

    Add Vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
